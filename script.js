4f09f0f1b3233fab2c9b407258f2cd9655465461
shilpa96-ice <narayanshilpa26@gmail.com>
2022-07-22 20:56:31 +0530

    simple bulb on/off project

---
+++ script.js
@@ -0,0 +1,11 @@
+function bulbon()
+{
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","bulbon.png");
+
+}
+function bulboff()
+{
+    let bulbobj = document.querySelector(".bulboff");
+    bulbobj.setAttribute("src","off.png");
+}
